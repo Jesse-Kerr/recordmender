f43cedaa56e4300fa145a9582c51e68ea0307b81
Jesse-Kerr <kerr.jesse@gmail.com>
2019-03-30 01:13:53 -0500

    Got all American djs from Wikipedia. Put function calls into whosampled_scrape.py

--- wikipedia_scrape.py
+++ wikipedia_scrape.py
@@ -1,30 +1,34 @@
 from time import sleep
 from pyvirtualdisplay import Display
 from selenium import webdriver
+import re
 
-def get_links_from_wikipage(all_artists):
+display = Display(visible=0, size=(1024, 768))
+display.start()
 
+driver = webdriver.Firefox()
+
+def get_links_from_wikipage():
     page_artists = driver.find_elements_by_xpath("//div[@class='mw-category-group']/ul/li/a")
-    page_artists = [artist.get_attribute('title') for artist in page_artists]
-    all_artists = all_artists + page_artists
-    return all_artists
+    return [artist.get_attribute('title') for artist in page_artists]
 
 def go_to_next_page():
     try:
         next_page = driver.find_element_by_link_text('next page')
+        driver.execute_script("arguments[0].scrollIntoView(true);", next_page)
         next_page.click()
+        return True
     except: 
         print("No more pages")
-        more_pages = False
+        return False
 
-if __name__ == "__main__":
+def get_all_artists():
     all_artists = []
     more_pages = True
-    driver = webdriver.Firefox()
     driver.get("https://en.wikipedia.org/wiki/Category:American_hip_hop_record_producers")
     while more_pages == True:
-        all_artists = get_links_from_wikipage(all_artists)
-        go_to_next_page()
-
-print(all_artists)
-print(len(all_artists))
+        page_artists = get_links_from_wikipage()
+        all_artists += page_artists
+        more_pages = go_to_next_page()
+    all_artists = [re.sub("[\(\[].*?[\)\]]", "", artist) for artist in all_artists]
+    return all_artists
