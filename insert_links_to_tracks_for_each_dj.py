ee935b19308a774dfd12897736d360f4b7660ea9
Jesse-Kerr <kerr.jesse@gmail.com>
2019-03-30 11:45:29 -0500

    Moved the set_more_pages command to top of for loop in try: except. Not sure if it was correctly resetting for each DJ. Likely messed up last AWS code

--- insert_links_to_tracks_for_each_dj.py
+++ insert_links_to_tracks_for_each_dj.py
@@ -7,13 +7,14 @@ if __name__ == "__main__":
     scraper.go_to_who_sampled_home_page()
     for dj in djs: 
         try:
+            scraper.set_more_pages_true()
             scraper.go_to_dj_page(dj)
             print("At {} page".format(dj))
             scraper.filter_page_by_songs_artist_sampled()
             scraper.get_num_samples_insert_mongo()
             while scraper.more_who_sampled_pages == True:
-                scraper.get_link_to_tracks_by_dj()
-                scraper.go_to_next_page()
+                scraper.get_link_to_tracks_by_dj_insert_mongo()
+                scraper.go_to_next_who_sampled_page()
         except:
             print("{} was unsuccessful!".format(dj))
     scraper.driver.quit()
