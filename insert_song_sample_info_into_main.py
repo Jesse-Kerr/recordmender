ea6cd5382b2accc25e679c76dda304ca01cb16d4
Jesse Kerr <jesse@Jesses-MacBook-Pro.local>
2019-04-03 09:55:21 -0500

    All three scraping methods currently functional

--- insert_song_sample_info_into_main.py
+++ insert_song_sample_info_into_main.py
@@ -6,10 +6,15 @@ from whosampled_scrape import Scraper
 
 if __name__ == "__main__":
     scraper = Scraper()
-    for song_sample_page in db.song_sample_pages: 
+    failed_links = []
+    song_sample_pages = db.song_sample_pages.distinct('link')
+    start = song_sample_pages.index("https://www.whosampled.com/sample/104267/Ayatollah-In-Time-My-Brother-Bill-Withers-Let-Me-Be-the-One-You-Need/")
+    for song_sample_page in song_sample_pages[start:23000]: 
         try:
             scraper.insert_song_sample_info_into_db_main(song_sample_page)
+            print('Done with {}'.format(song_sample_page))
         except:
-            print("Insertion into Mongo failed for {}".format(link))
-    scraper.get_distinct_from_song_sample_pages_db()
+            print("Insertion into Mongo failed for {}".format(song_sample_page))
+            failed_links.append(song_sample_page)
+    print(failed_links)
     scraper.driver.quit()
