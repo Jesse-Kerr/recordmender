5c5bb6c2e34d0440eab46018ef7e6f586dc070ee
Jesse Kerr <jesse@Jesses-MacBook-Pro.local>
2019-04-04 14:02:07 -0500

    Cleaned data to decrease sparsity

--- turn_db_main_into_utility_matrix.py
+++ turn_db_main_into_utility_matrix.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 client = MongoClient()
 db = client.whosampled
+import re
 
 import pandas as pd
 
@@ -26,20 +27,31 @@ def from_mongo_collection_to_utility_matrix(mongo_collection):
     #Turn sampled_song_producer from list to string
     df['sampled_song_producer'] = df.sampled_song_producer.apply( lambda x: ', '.join(x))
 
-    #Create new column with both sampled_artist and sampled_name
-    df['sampled_artist_and_name'] = "{} - {}".format(df['sampled_artist'], df['sampled_song_name']) 
-    
+    #Create new column with both sampled_artist and sampled_name    
     #Remove any possible duplicates
-    df.drop_duplicates(['new_song_name', 'new_song_producer', 'sampled_artist_and_name'], inplace=True)
+    df = df.drop_duplicates(['URL', 'new_song_producer'])
 
+    df['new_song_producer'] = df.new_song_producer.apply(lambda x: re.sub('\(.*\)', '', x))
     # Make utility matrix: Get lists of producers and songs
-    sampled_songs = list(df.drop_duplicates('sampled_song_name')['sampled_song_name'])
-    new_song_producers = list(df.drop_duplicates('new_song_producer')['new_song_producer'])
+    #sampled_songs = list(df.drop_duplicates('sampled_song_name')['sampled_song_name'])
+    #new_song_producers = list(df.drop_duplicates('new_song_producer')['new_song_producer'])
 
     # Make new dict with zeros everywhere using the columns and rows above.
-    utility_matrix = pd.DataFrame(0, index=sampled_songs, columns= new_song_producers).to_dict()
+    #utility_matrix = pd.DataFrame(0, index=sampled_songs, columns= new_song_producers).to_dict()
+    df.sampled_song_name = df.sampled_song_name\
+    .apply(lambda x: x.strip())\
+    .apply(lambda x: x.lower())\
+    .apply(lambda x: re.sub('\(.* version\)$|instrumental$', '', x))\
+    .apply(lambda x: re.sub('\(live\)|\(.* remix\)', '', x))\
+    .apply(lambda x: re.sub('\(version .*', '', x))\
+    .apply(lambda x: re.sub('\(|\)|"|\'', '', x))\
+    .apply(lambda x: re.sub('-', ' ', x))\
+    .apply(lambda x: x.strip())\
+    .apply(lambda x: re.sub(' +', ' ', x))
 
-    make_util(utility_matrix, df)
-    
-    utility2 = pd.DataFrame.from_dict(utility_matrix, orient = 'index')
-    return utility2
+    df['sampled_artist_song'] = df.sampled_artist + ' - ' + df.sampled_song_name
+
+    # make_util(utility_matrix, df)
+    utility = pd.crosstab(df['new_song_producer'], columns=df.sampled_artist_song)
+    # utility2 = pd.DataFrame.from_dict(utility_matrix, orient = 'index')
+    return utility, df
