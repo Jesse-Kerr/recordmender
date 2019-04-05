9c2f4328851d51ca880d1064b66aec4a16f8cc39
Jesse Kerr <jesse@Jesses-MBP.lan1>
2019-04-05 08:26:44 -0500

    Put in form for debugging

--- src/auc_testing.py
+++ src/auc_testing.py
@@ -1,3 +1,37 @@
+from sklearn import metrics
+import numpy as np
+import scipy.sparse as sparse
+import implicit
+
+from pymongo import MongoClient
+client = MongoClient()
+db = client.whosampled
+
+from turn_db_main_into_utility_matrix import from_mongo_collection_to_utility_matrix
+_, utility_to_artist, _ = from_mongo_collection_to_utility_matrix(db.main_redo)
+
+utility_to_artist_sparse = sparse.csr_matrix(utility_to_artist.T)
+
+from make_train_set import make_train
+train_set, test_set, user_rows_altered = make_train(utility_to_artist_sparse)
+
+def auc_score(predictions, test):
+    '''
+    This simple function will output the area under the curve using sklearn's metrics. 
+    
+    parameters:
+    
+    - predictions: your prediction output
+    
+    - test: the actual target result you are comparing to
+    
+    returns:
+    
+    - AUC (area under the Receiver Operating Characterisic curve)
+    '''
+    fpr, tpr, thresholds = metrics.roc_curve(test, predictions)
+    return metrics.auc(fpr, tpr)
+
 def calc_mean_auc(training_set, altered_users, predictions, test_set):
     '''
     This function will calculate the mean AUC by user for any user that had their user-item matrix altered. 
@@ -14,15 +48,12 @@ def calc_mean_auc(training_set, altered_users, predictions, test_set):
     
     test_set - The test set constucted earlier from make_train function
     
-    
-    
     returns:
     
     The mean AUC (area under the Receiver Operator Characteristic curve) of the test set only on user-item interactions
     there were originally zero to test ranking ability in addition to the most popular items as a benchmark.
     '''
-    
-    
+
     store_auc = [] # An empty list to store the AUC for each user that had an item removed from the training set
     popularity_auc = [] # To store popular AUC scores
     pop_items = np.array(test_set.sum(axis = 0)).reshape(-1) # Get sum of item iteractions to find most popular
@@ -45,3 +76,12 @@ def calc_mean_auc(training_set, altered_users, predictions, test_set):
     
     return float('%.3f'%np.mean(store_auc)), float('%.3f'%np.mean(popularity_auc))  
    # Return the mean AUC rounded to three decimal places for both test and popularity benchmark
+
+
+alpha = 15
+user_vecs, item_vecs = implicit.alternating_least_squares((train_set*alpha).astype('double'), 
+                                                          factors=20, 
+                                                          regularization = 0.1, 
+                                                         iterations = 50)
+
+calc_mean_auc(train_set, user_rows_altered, sparse.csr_matrix(user_vecs), test_set)
