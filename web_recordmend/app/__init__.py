d031fc9b6789fc1c373ea343cad823b6951cdc3f
Jesse Kerr <jesse@Jesses-MBP.lan1>
2019-04-09 08:27:40 -0500

    Creating Basic website templates

--- web_recordmend/app/__init__.py
+++ web_recordmend/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
-
+from config import Config
+import os
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 from app import routes
--- web_recordmend/app/routes.py
+++ web_recordmend/app/routes.py
@@ -1,16 +1,24 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from flask_pymongo import PyMongo
 from app import app
+from app.forms import LoginForm
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/whosampled"
 mongo = PyMongo(app)
 
 @app.route("/")
-def home_page():
-    dj = mongo.db.dj_meta_info.find_one()
-    return render_template("index.html",
-        dj=dj)
-
 @app.route("/index")
 def index():
-    return "index"
+    dj = mongo.db.dj_meta_info.find_one({}, {"dj": 1, "_id": 0})
+    djs = mongo.db.dj_meta_info.find({}, {"dj": 1, "_id": 0}).limit(5)
+    return render_template("index.html",
+        dj=dj, djs =djs)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login Requested for {}, remember_me = {}'.format(
+                form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title = 'Sign In', form = form)
