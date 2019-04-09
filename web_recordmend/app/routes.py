40152f44bf6fcd5ebd32c38abe8ae04adde2e3d5
Jesse Kerr <jesse@Jesses-MBP.lan1>
2019-04-09 09:41:15 -0500

    All templates

--- web_recordmend/app/routes.py
+++ web_recordmend/app/routes.py
@@ -1,12 +1,25 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from flask_pymongo import PyMongo
 from app import app
 from app.forms import LoginForm
+import pandas as pd
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/whosampled"
 mongo = PyMongo(app)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
+def dropdown():
+    df = pd.DataFrame(list(mongo.db.main_redo.find()))
+    df = df[(df.new_song_producer != 'None Listed') & (df.sampled_artist != 'None Listed') & (df.sampled_song_name != 'None Listed')]
+    top_twenty_djs = list(df.groupby('new_song_producer').count()['URL'].sort_values(ascending = False)[:20].index)
+    return render_template('test.html', djs=top_twenty_djs)
+
+@app.route("/submitted", methods = ['GET', 'POST'])
+def hello():
+    var = request.form.get("djs")
+    return render_template('index.html', var=var)
+
+
 @app.route("/index")
 def index():
     dj = mongo.db.dj_meta_info.find_one({}, {"dj": 1, "_id": 0})
---
+++ web_recordmend/turn_db_main_into_utility_matrix.py
@@ -0,0 +1,56 @@
+from pymongo import MongoClient
+client = MongoClient()
+db = client.whosampled
+import re
+
+import pandas as pd
+
+                
+def from_mongo_collection_to_utility_matrix(mongo_collection):
+    '''
+    Takes data from mongo_Collection and returns a pandas dataframe as a utility matrix
+    '''
+    
+    df = pd.DataFrame(list(mongo_collection.find()))
+
+    #Turn sampled_song_producer from list to string
+    df['sampled_song_producer'] = df.sampled_song_producer.apply( lambda x: ', '.join(x))
+
+    #Remove any possible duplicates
+    df = df.drop_duplicates(['URL', 'new_song_producer'])
+
+    #drop rows with none_listed for essential columns
+    #Only lists 100 out of 17,000 times there is no new_song_producer, similar low amounts for other columns below.
+
+    df = df[(df.new_song_producer != 'None Listed') & (df.sampled_artist != 'None Listed') & (df.sampled_song_name != 'None Listed')]
+
+    #Need to remove this step for now. 
+    df['new_song_producer'] = df.new_song_producer.apply(lambda x: re.sub('\(.*\)', '', x))
+
+    # Make utility matrix: Get lists of producers and songs
+    #sampled_songs = list(df.drop_duplicates('sampled_song_name')['sampled_song_name'])
+    #new_song_producers = list(df.drop_duplicates('new_song_producer')['new_song_producer'])
+
+    # Make new dict with zeros everywhere using the columns and rows above.
+    #utility_matrix = pd.DataFrame(0, index=sampled_songs, columns= new_song_producers).to_dict()
+    df.sampled_song_name = df.sampled_song_name\
+    .apply(lambda x: x.strip())\
+    .apply(lambda x: x.lower())\
+    .apply(lambda x: re.sub('\(.* version\)$|instrumental$', '', x))\
+    .apply(lambda x: re.sub('\(live\)|\(.* remix\)', '', x))\
+    .apply(lambda x: re.sub('\(version .*', '', x))\
+    .apply(lambda x: re.sub('\(|\)|"|\'', '', x))\
+    .apply(lambda x: re.sub('-', ' ', x))\
+    .apply(lambda x: x.strip())\
+    .apply(lambda x: re.sub(' +', ' ', x))
+
+    df.sampled_artist = df.sampled_artist.apply(lambda x: re.sub('\(.*\)', '', x))
+    
+    #Create new column with both sampled_artist and sampled_name    
+    df['sampled_artist_song'] = df.sampled_artist + ' - ' + df.sampled_song_name
+
+    # make_util(utility_matrix, df)
+    utility_to_song = pd.crosstab(df.sampled_artist_song, columns=df.new_song_producer)
+    utility_to_artist = pd.crosstab(df.sampled_artist, columns=df.new_song_producer )
+
+    return utility_to_song, utility_to_artist, df
